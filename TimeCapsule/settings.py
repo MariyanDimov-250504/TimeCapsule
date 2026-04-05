31ac557a6b53f20f9427cfd8f0a7ac81533ca25d
Mariyan R. Dimov <jalopy738.sp56420@gmail.com>
2026-04-05 13:06:26 +0300

    Proj Update

--- TimeCapsule/settings.py
+++ TimeCapsule/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+import dj_database_url
 # Load environment variables
 load_dotenv()
 
@@ -85,16 +85,21 @@ WSGI_APPLICATION = 'TimeCapsule.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'timecapsule_db'),
-        'USER': os.getenv('DB_USER', 'timecapsule_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'timecapsule123'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
 
 
 # Password validation
